9803a680d15dde99ae4516f32acf526a92285840
Anna Panas <annapanas0906@gmail.com>
2020-05-19 21:16:16 -0400

    added github links to projects

--- src/pages/Bidbot.js
+++ src/pages/Bidbot.js
@@ -1,87 +1,153 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { FaArrowRight } from 'react-icons/fa';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { FaArrowRight } from "react-icons/fa";
 
 function Bidbot() {
+  AOS.init({
+    duration: 1200,
+  });
 
-    AOS.init({
-        duration: 1200,
-    })
+  return (
+    <>
+      <Navbar />
 
-    return (
-        <>
-            <Navbar />
-         
-            <div className="container">
-            <br></br><br></br> 
-            <h6 className="projectInfo">#School group project built with MongoDB, Express, React.js, Node.js#</h6>
-                <li className="min"> 5 min read</li><br></br><br></br>
-                <div className="row">
-                    <div className="col-md-5 col-sm-12">
-                        <br></br><br></br>
-                        <p>Online auctions have  picked up and grown alongside the thriving digital world. They allow extreme convenience for bidders, as they don't have to leave their homes, but can instead just open up the browser and find their desired items. </p>
-                    </div>
+      <div className="container">
+        <br></br>
+        <br></br>
+        <h6 className="projectInfo">
+          #School group project built with MongoDB, Express, React.js, Node.js#
+        </h6>
+        <li className="min"> 5 min read</li>
+        <br></br>
+        <br></br>
+        <div className="row">
+          <div className="col-md-5 col-sm-12">
+            <br></br>
+            <br></br>
+            <p>
+              Online auctions have picked up and grown alongside the thriving
+              digital world. They allow extreme convenience for bidders, as they
+              don't have to leave their homes, but can instead just open up the
+              browser and find their desired items.{" "}
+            </p>
+          </div>
 
-                    <div className="col-md-6 offset-sm-1 col-sm-12">
-                        <div className="card" data-aos="fade-left" data-aos-once="true">
-                            <img src="/images/bot1.png" width="100%" height="300px" alt="bidbot"/>
-                        </div>
-                    </div>
-
-                </div>
-                <br></br><br></br><br></br><br></br>
-
-                <div className="row">
-                    <div className="col-md-6  col-sm-12">
-                        <div className="card">
-                            <img src="/images/bid.png" width="100%" height="300px" alt="bidbot"/>
-                        </div>
-                    </div>
-                    <div className="col-md-5 offset-sm-1 col-sm-12">
-                        <p>Just make yourself a cup of coffee and find your favorite pieces in different categories while still wearing pajamas</p>
-                        <div className="card" data-aos="fade-up" data-aos-once="true">
-                            <img src="/images/biditems.png" width="100%" height="200px" alt="bidbot"/>
-                        </div>
-                    </div>
-                </div>
-                <br></br><br></br><br></br><br></br>
-                <h2>Start bidding once you found it </h2>
-                <br></br>
-                <div className="row">
-                 
-                    <div className="col-md-6 offset-sm-3 col-sm-12">
-                    <div className="card" data-aos="flip-down" data-aos-once="true">
-                            <img src="/images/b2.png" width="100%" height="300px" alt="bidbot"/>
-                        </div>
-                    </div>
-                    
-                </div>
-                <br></br><br></br><br></br>
-                <h2>Not willing to buy, but have something to sell?</h2>
-                <br></br><br></br><br></br>
-                <div className="row">
-                    <div className="col-md-5 offset-sm-1 col-sm-12">
-                        <br></br><br></br><br></br>
-                    <p data-aos="zoom-out-down" data-aos-once="true">Online auctions are  benefitial  to sellers, because sellers don't have to pay fees to hold an auction event in a certain location. Just post your item and make some extra cash <span className="arrow"><FaArrowRight /></span></p>
-                    </div>
-
-                    <div className="col-md-3 offset-sm-1 col-sm-12">
-                    <div className="card" >
-                            <img src="/images/post.png" width="100%" height="400px" alt="bidbot"/>
-                        </div>
-                    </div>
-                    
-               
-                <br></br><br></br><br></br>
+          <div className="col-md-6 offset-sm-1 col-sm-12">
+            <div className="card" data-aos="fade-left" data-aos-once="true">
+              <img
+                src="/images/bot1.png"
+                width="100%"
+                height="300px"
+                alt="bidbot"
+              />
             </div>
+          </div>
+        </div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+
+        <div className="row">
+          <div className="col-md-6  col-sm-12">
+            <div className="card">
+              <img
+                src="/images/bid.png"
+                width="100%"
+                height="300px"
+                alt="bidbot"
+              />
             </div>
-            <br></br><br></br>
-            <Footer />
-        </>
-    )
+          </div>
+          <div className="col-md-5 offset-sm-1 col-sm-12">
+            <p>
+              Just make yourself a cup of coffee and find your favorite pieces
+              in different categories while still wearing pajamas
+            </p>
+            <div className="card" data-aos="fade-up" data-aos-once="true">
+              <img
+                src="/images/biditems.png"
+                width="100%"
+                height="200px"
+                alt="bidbot"
+              />
+            </div>
+          </div>
+        </div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <h2>Start bidding once you found it </h2>
+        <br></br>
+        <div className="row">
+          <div className="col-md-6 offset-sm-3 col-sm-12">
+            <div className="card" data-aos="flip-down" data-aos-once="true">
+              <img
+                src="/images/b2.png"
+                width="100%"
+                height="300px"
+                alt="bidbot"
+              />
+            </div>
+          </div>
+        </div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <h2>Not willing to buy, but have something to sell?</h2>
+        <br></br>
+        <br></br>
+        <br></br>
+        <div className="row">
+          <div className="col-md-5 offset-sm-1 col-sm-12">
+            <br></br>
+            <br></br>
+            <br></br>
+            <p data-aos="zoom-out-down" data-aos-once="true">
+              Online auctions are benefitial to sellers, because sellers don't
+              have to pay fees to hold an auction event in a certain location.
+              Just post your item and make some extra cash{" "}
+              <span className="arrow">
+                <FaArrowRight />
+              </span>
+            </p>
+          </div>
+
+          <div className="col-md-3 offset-sm-1 col-sm-12">
+            <div className="card">
+              <img
+                src="/images/post.png"
+                width="100%"
+                height="400px"
+                alt="bidbot"
+              />
+            </div>
+          </div>
+
+          <br></br>
+          <br></br>
+          <br></br>
+        </div>
+      </div>
+      <br></br>
+      <br></br>
+      <a
+        href="https://github.com/vormc2005/Project3"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="projectInfo"
+      >
+        #See project on GitHub#
+      </a>
+      <br></br>
+      <br></br>
+      <Footer />
+    </>
+  );
 }
 
-export default Bidbot
+export default Bidbot;
--- src/pages/Shopdesk.js
+++ src/pages/Shopdesk.js
@@ -1,132 +1,224 @@
 import React from "react";
-import "./shopdesk.css"
+import "./shopdesk.css";
 
 // react-bootstrap
 import Navbar from "../components/Navbar/index";
-import Footer from '../components/Footer/index';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import Footer from "../components/Footer/index";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function Shopdesk() {
+  AOS.init({
+    duration: 1200,
+  });
 
-    AOS.init({
-        duration: 1200,
-    })
+  return (
+    <>
+      <Navbar />
 
-    return (
+      <div className="container">
+        <br></br>
+        <h6 className="projectInfo">
+          #School group project built with Node.js, Express, Handlebars, MySQL#
+        </h6>
+        <li className="min"> 4 min read</li>
+        <br></br>
 
-        <>
-            <Navbar />
-
-
-            <div className="container">
-                <br></br>
-                <h6 className="projectInfo">#School group project built with Node.js, Express, Handlebars, MySQL#</h6>
-                <li className="min"> 4 min read</li>
-                <br></br>
-
-                <div className="row">
-                    <div className="col-12">
-                       
-                            <h2>E-commerce is the future</h2>
-                    
-                    </div>
-                </div>
-                <br></br><br></br>
-                <div className="row">
-
-                    <div className="col-md-5 col-sm-12">
-                        <br></br><br></br><br></br>
-                        <p className="p"> It's growing five times faster than retail. Small businesses can't keep up being forced to lose out on sales or accept fees and brand dilution by selling their products on Amazon. </p>
-
-                    </div>
-                    <div className="col-md-6 offset-sm-1 col-sm-12">
-
-                        <div className="card" data-aos="zoom-in" data-aos-once="true">
-                            <img src="/images/s1.png" width="100%" height="280px" alt="shopdesk" />
-                        </div>
-                    </div>
-                </div>
-                <br></br><br></br><br></br><br></br>
-                <div className="row">
-                    <div className="col-md-6 col-sm-12">
-                        <div className="card" data-aos="fade-right" data-aos-once="true">
-                            <img src="/images/s2.png" width="100%" height="280px" alt="shopdesk" />
-                        </div>
-                    </div>
-
-                    <div className="col-md-6  col-sm-12">
-                        <br></br><br></br><br></br>
-                        <p className="p">Small businesses can't compete with giants like Amazon and Wallmart</p>
-                    </div>
-
-                </div>
-                <br></br><br></br><br></br><br></br>
-
-
-
-                <h2>We don't show our name on your website, so they only remember your unique brand</h2>
-                <br></br><br></br>
-                <div className="row">
-
-                    <div className="col-md-6 offset-sm-1 col-sm-12">
-                        <div className="card" >
-                            <img src="/images/shopdesk.png" width="100%" height="200px" alt="shopdesk" />
-                        </div>
-                        <br></br>
-                        <div className="card" >
-                            <img src="/images/bedroom.png" width="100%" height="200px" alt="shopdesk" />
-                        </div>
-                    </div>
-                    <div className="col-md-3 offset-sm-1 col-sm-12">
-                        <br></br>
-                        <div className="card" data-aos="flip-right" data-aos-once="true">
-                            <img src="/images/cart.png" width="100%" height="380px" alt="shopdesk" />
-                        </div>
-                    </div>
-                </div>
-                <br></br><br></br><br></br><br></br>
-                <h2>Cloud based inventory management lets you keep track of your store from anywhere anytime </h2>
-                <br></br><br></br><br></br><br></br>
-                <div className="row">
-                    <div className="col-md-5 offset-sm-1 col-sm-12">
-                        <div className="card" data-aos="zoom-in" data-aos-once="true">
-                            <img src="/images/s4.png" width="100%" height="300px" alt="shopdesk" />
-                        </div>
-                    </div>
-                    <div className="col-md-5 col-sm-12">
-                        <div className="card" data-aos="zoom-in" data-aos-once="true">
-                            <img src="/images/s5.png" width="100%" height="300px" alt="shopdesk" />
-                        </div>
-                    </div>
-                </div>
-                <br></br><br></br><br></br><br></br>
-                <h2>Keeping user's information safe is our highest priority</h2>
-                <br></br><br></br>
-                <div className="row">
-                    <div className="col-md-3 offset-sm-2 col-sm-12">
-                        <div className="card" data-aos="flip-left" data-aos-once="true">
-                            <img src="/images/s3.png" width="100%" height="350px" alt="shopdesk" />
-                        </div>
-                    </div>
-                    <div className="col-md-6 col-sm-12">
-                        <br></br><br></br><br></br><br></br>
-                        <p className="p">We use a secure login system to authenticate  and persist users and shopping carts </p>
-                    </div>
-                </div>
-                <br></br><br></br><br></br>
-                <h2>Don't get left behind</h2>
-                <br></br><br></br>
-                <div className="col-md-8 offset-sm-2 col-sm-12">
-                    <div className="card" data-aos="flip-down" data-aos-once="true">
-                        <img src="/images/shop.png" width="100%" height="250px" alt="shopdesk" />
-                    </div>
-                </div>
-                <br></br><br></br><br></br>
+        <div className="row">
+          <div className="col-12">
+            <h2>E-commerce is the future</h2>
+          </div>
+        </div>
+        <br></br>
+        <br></br>
+        <div className="row">
+          <div className="col-md-5 col-sm-12">
+            <br></br>
+            <br></br>
+            <br></br>
+            <p className="p">
+              {" "}
+              It's growing five times faster than retail. Small businesses can't
+              keep up being forced to lose out on sales or accept fees and brand
+              dilution by selling their products on Amazon.{" "}
+            </p>
+          </div>
+          <div className="col-md-6 offset-sm-1 col-sm-12">
+            <div className="card" data-aos="zoom-in" data-aos-once="true">
+              <img
+                src="/images/s1.png"
+                width="100%"
+                height="280px"
+                alt="shopdesk"
+              />
             </div>
-            <Footer />
-        </>
-    )
+          </div>
+        </div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <div className="row">
+          <div className="col-md-6 col-sm-12">
+            <div className="card" data-aos="fade-right" data-aos-once="true">
+              <img
+                src="/images/s2.png"
+                width="100%"
+                height="280px"
+                alt="shopdesk"
+              />
+            </div>
+          </div>
+
+          <div className="col-md-6  col-sm-12">
+            <br></br>
+            <br></br>
+            <br></br>
+            <p className="p">
+              Small businesses can't compete with giants like Amazon and
+              Wallmart
+            </p>
+          </div>
+        </div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+
+        <h2>
+          We don't show our name on your website, so they only remember your
+          unique brand
+        </h2>
+        <br></br>
+        <br></br>
+        <div className="row">
+          <div className="col-md-6 offset-sm-1 col-sm-12">
+            <div className="card">
+              <img
+                src="/images/shopdesk.png"
+                width="100%"
+                height="200px"
+                alt="shopdesk"
+              />
+            </div>
+            <br></br>
+            <div className="card">
+              <img
+                src="/images/bedroom.png"
+                width="100%"
+                height="200px"
+                alt="shopdesk"
+              />
+            </div>
+          </div>
+          <div className="col-md-3 offset-sm-1 col-sm-12">
+            <br></br>
+            <div className="card" data-aos="flip-right" data-aos-once="true">
+              <img
+                src="/images/cart.png"
+                width="100%"
+                height="380px"
+                alt="shopdesk"
+              />
+            </div>
+          </div>
+        </div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <h2>
+          Cloud based inventory management lets you keep track of your store
+          from anywhere anytime{" "}
+        </h2>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <div className="row">
+          <div className="col-md-5 offset-sm-1 col-sm-12">
+            <div className="card" data-aos="zoom-in" data-aos-once="true">
+              <img
+                src="/images/s4.png"
+                width="100%"
+                height="300px"
+                alt="shopdesk"
+              />
+            </div>
+          </div>
+          <div className="col-md-5 col-sm-12">
+            <div className="card" data-aos="zoom-in" data-aos-once="true">
+              <img
+                src="/images/s5.png"
+                width="100%"
+                height="300px"
+                alt="shopdesk"
+              />
+            </div>
+          </div>
+        </div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <h2>Keeping user's information safe is our highest priority</h2>
+        <br></br>
+        <br></br>
+        <div className="row">
+          <div className="col-md-3 offset-sm-2 col-sm-12">
+            <div className="card" data-aos="flip-left" data-aos-once="true">
+              <img
+                src="/images/s3.png"
+                width="100%"
+                height="350px"
+                alt="shopdesk"
+              />
+            </div>
+          </div>
+          <div className="col-md-6 col-sm-12">
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <p className="p">
+              We use a secure login system to authenticate and persist users and
+              shopping carts{" "}
+            </p>
+          </div>
+        </div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <h2>Don't get left behind</h2>
+        <br></br>
+        <br></br>
+        <div className="col-md-8 offset-sm-2 col-sm-12">
+          <div className="card" data-aos="flip-down" data-aos-once="true">
+            <img
+              src="/images/shop.png"
+              width="100%"
+              height="250px"
+              alt="shopdesk"
+            />
+          </div>
+        </div>
+
+        <br></br>
+        <br></br>
+      </div>
+      <a
+        href="https://github.com/magiama9/shopdesk"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="projectInfo"
+      >
+        #See project on GitHub#
+      </a>
+      <br></br>
+      <br></br>
+      <Footer />
+    </>
+  );
 }
 
 export default Shopdesk;
