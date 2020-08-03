bf4cab5c711c891915a8cdd1a1d5eba80bcc3bf8
Anna Panas <annapanas0906@gmail.com>
2020-08-02 23:17:54 -0400

    edits to project

--- src/pages/Ecommerce.js
+++ src/pages/Ecommerce.js
@@ -16,13 +16,12 @@ const Ecommerce = () => {
         <br></br>
         <br></br>
         <h6 className="projectInfo">
-          #Project built with MongoDB, Express, React.js, Node.js, Firebase,
-          Nodemailer#
+          #MongoDB, Express, React.js, Node.js, Firebase, Nodemailer#
         </h6>
         <li className="min"> 5 min read</li>
         <br></br>
         <div className="row">
-          <div className="col-md-6">
+          <div className="col-md-6 col-sm-12">
             <p>
               Kitchen Land is an online store that sells products in following
               categories: Blenders, Juicers, Oil Presses and Dehydrators. Each
@@ -37,7 +36,7 @@ const Ecommerce = () => {
               data-aos-once="true"
             />
           </div>
-          <div className="col-md-6 col-sm-12">
+          <div className="col-md-5 offset-sm-1 col-sm-12">
             <img src="/images/kl-home.png" width="100%" height="280px" />
             <p>
               <br></br>
@@ -47,13 +46,13 @@ const Ecommerce = () => {
           </div>
         </div>
         <div className="row">
-          <div className="col-md-4 offset-sm-1 col-sm-12">
-            <br></br> <br></br>
-            <br></br>
-            <img src="/images/user-cart.png" width="100%" height="280px" />
+          <div className="col-md-6  col-sm-12">
+            <br></br> <br></br> <br></br> <br></br>
+            <img src="/images/user-cart.png" width="100%" height="400px" />
           </div>
           <div className="col-md-5 offset-sm-1 col-sm-12">
             <p>
+              <br></br> <br></br> <br></br> <br></br>
               <br></br>
               Secure login system provides reliable user authentication and
               shopping cart management.
@@ -75,6 +74,8 @@ const Ecommerce = () => {
             data-aos="fade-right"
             data-aos-once="true"
           >
+            <br></br>
+            <br></br>
             <p>
               Cloud based management lets admin keep track of inventory from
               anywhere anytime.
@@ -86,6 +87,8 @@ const Ecommerce = () => {
             data-aos="fade-left"
             data-aos-once="true"
           >
+            <br></br>
+            <br></br>
             <img src="/images/40.png" width="100%" height="280px" />
             <p>
               Access Control feature restricts any users other than admin from
@@ -101,13 +104,14 @@ const Ecommerce = () => {
             data-aos="flip-left"
             data-aos-once="true"
           >
-            <br></br>
+            <br></br> <br></br> <br></br> <br></br> <br></br> <br></br>
             <p>
               Customer service is our top priority. Users can contact the store
               regarding any questions by simply filling out a form.
             </p>
           </div>
           <div className="col-md-3 offset-sm-1 col-sm-12">
+            <br></br> <br></br> <br></br>
             <img src="/images/contact.png" width="100%" height="280px" />
           </div>
         </div>
